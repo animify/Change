b3314b6310720d905b8c755424391dd4f3dc91c4
Stefan Mansson <stefan.aotik@gmail.com>
2020-01-19 13:54:46 +0100

    refactor: add commit helpers

--- src/lib/Commits.ts
+++ src/lib/Commits.ts
@@ -1,4 +1,29 @@
+import semver from "semver";
+import execa from "execa";
+
 export default class Commits {
-  public getLastTag() {}
+  constructor() {
+    if (typeof module !== "undefined" && module.exports) {
+      throw new Error(
+        "Commits class can only be created in a Node environment."
+      );
+    }
+  }
+
+  public async getLatestTag() {
+    const { stdout } = await execa("git", ["describe", "--abbrev=0", "--tags"]);
+    return stdout;
+  }
+
   public getCommits() {}
+
+  public async isWorkingTreeClean() {
+    try {
+      const { stdout: status } = await execa("git", ["status", "--porcelain"]);
+
+      return status === "";
+    } catch (_) {
+      return false;
+    }
+  }
 }
